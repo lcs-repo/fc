1ef14604f7a3818eb3d15ef2100af40d6a71c6da
lcs-repo <lcs.fpt.ipo@gmail.com>
2024-09-09 20:34:51 +0800

    Additional questions

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,16 @@ import { useState } from 'react';
 import Flashcard from '../components/Flashcard';
 
 const flashcards = [
-  { question: "What is the capital of France?", answer: "Paris" },
-  { question: "What is 2 + 2?", answer: "4" },
-  { question: "Who wrote 'Romeo and Juliet'?", answer: "William Shakespeare" },
+  { question: "Teratogen: Renal Damage", answer: "ACE inhibitors" },
+  { question: "Teratogen: Hypocalvaria", answer: "ACE inhibitors" },
+  { question: "Teratogen: Multiple major malformations", answer: "Aminopterin" },
+  { question: "Teratogen: Suspected abnormal developmental patterns", answer: "Amphetamines" },
+  { question: "Teratogen: Decreased School performance", answer: "Amphetamines" },
+  { question: "Teratogen: Masculinization of female fetus", answer: "Androgens" },
+  { question: "Teratogen: Neonatal withdrawal symptoms have been reported ina few cases with clomipramine, desipramine, and imipramine", answer: "Antidepressants, TCA" },
+  { question: "Teratogen: Chronic use can lead to neonatal dependence", answer: "Barbiturates" },
+  { question: "Teratogen: Various major malformations", answer: "Busulfan" },
+  { question: "Teratogen: Neural Tube Defects", answer: "Carbamazepine" },
 ];
 
 export default function Home() {
